b644a80aa98e4ac9bb60de4ba8d85b6cde1221b6
Celestino Bellone <celestino@exteso.com>
2016-01-31 20:00:06 +0100

    add missing license headers

--- app/src/main/java/alfio/backoffice/ManualInsertActivity.kt
+++ app/src/main/java/alfio/backoffice/ManualInsertActivity.kt
@@ -1,3 +1,19 @@
+/**
+ * This file is part of alf.io backoffice.
+
+ * alf.io backoffice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * alf.io backoffice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with alf.io backoffice.  If not, see //www.gnu.org/licenses/>.
+ */
 package alfio.backoffice
 
 import android.os.Bundle
