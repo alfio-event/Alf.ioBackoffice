d626cee72cad8d3244340b8ae924e3fae179187c
Celestino Bellone <celestino@exteso.com>
2016-02-08 21:53:28 +0100

    add third party licenses

--- app/src/main/java/alfio/backoffice/AboutActivity.kt
+++ app/src/main/java/alfio/backoffice/AboutActivity.kt
@@ -17,9 +17,12 @@
 package alfio.backoffice
 
 import android.os.Bundle
+import android.text.Html
 import android.text.method.LinkMovementMethod
+import android.util.Xml
 import kotlinx.android.synthetic.main.app_bar.*
 import kotlinx.android.synthetic.main.content_about.*
+import org.xmlpull.v1.XmlPullParser
 
 class AboutActivity : BaseActivity() {
 
@@ -32,5 +35,39 @@ class AboutActivity : BaseActivity() {
         val version = packageManager.getPackageInfo(packageName, 0).versionName;
         license.movementMethod = LinkMovementMethod.getInstance();
         appVersion.text = "$appName v.$version";
+        val parser = Xml.newPullParser();
+        parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(resources.assets.open("dependency-license.xml"), null);
+        parser.nextTag();
+        parser.require(XmlPullParser.START_TAG, null, "dependencies");
+        thirdparty.text = Html.fromHtml(getElementsByTagName("dependency")(parser)
+                .filter { it.url != null }
+                .joinToString(separator = "<br/><br/>", transform = {"${it.file}:<br/><a href=\"${it.url}\">${it.license}</a>"}));
+        thirdparty.movementMethod = LinkMovementMethod.getInstance();
+    }
+
+    fun getElementsByTagName(name: String) : (XmlPullParser) -> MutableList<Item> {
+        return {
+            it.nextTag();
+            it.require(XmlPullParser.START_TAG, null, name);
+            val result: MutableList<Item> = arrayListOf();
+            while(it.next() != XmlPullParser.END_DOCUMENT) {
+                if (it.eventType == XmlPullParser.START_TAG && it.name.equals("dependency")) {
+                    result.add(readElement(it));
+                }
+            }
+            result;
+        };
+    }
+
+    fun readElement(parser: XmlPullParser) : Item {
+        parser.nextTag();
+        val fileName = parser.nextText();
+        parser.nextTag();
+        val license = parser.getAttributeValue(0);
+        val url = if(parser.attributeCount > 1) parser.getAttributeValue(1) else null;
+        return Item(fileName, license, url);
     }
 }
+
+data class Item(val file: String, val license: String, val url: String?);
