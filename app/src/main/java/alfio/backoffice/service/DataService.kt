5a55b815ec8809faa32768925d112c7bb1166d5f
Celestino Bellone <celestino@exteso.com>
2016-03-17 18:35:14 +0100

    #4 - refactoring + first draft

--- app/src/main/java/alfio/backoffice/service/DataService.kt
+++ app/src/main/java/alfio/backoffice/service/DataService.kt
@@ -24,12 +24,15 @@ import android.preference.PreferenceManager
 import com.google.gson.reflect.TypeToken
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
 
 object DataService {
 
     private val sharedPreferences: SharedPreferences;
     private val blacklist: MutableSet<AlfioConfiguration> = hashSetOf();
-    val configurations: ConcurrentHashMap<String, AlfioConfiguration>;
+    private val pendingSponsorScanQueue: Queue<Pair<AlfioConfiguration, String>>;
+    private val inProcessScan: MutableList<Pair<AlfioConfiguration, String>>;
+    val configurations: MutableMap<String, AlfioConfiguration>;
     val alfioConfigurations: MutableList<AlfioConfiguration>
         get() = ArrayList(configurations.values.filter { !blacklist.contains(it) });
     val vibrationFeedbackEnabled: Boolean
@@ -39,12 +42,18 @@ object DataService {
 
     init {
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(AlfioBackoffice.context);
-        val existing: Map<String, AlfioConfiguration>? = Common.gson.fromJson(sharedPreferences.getString(KEY_ALFIO_CONFIGURATIONS, ""), SerializedConfigurations().type);
-        this.configurations = if(existing != null) ConcurrentHashMap(existing) else ConcurrentHashMap();
+        this.configurations = loadSavedValue(KEY_ALFIO_CONFIGURATIONS, SerializedConfigurations(), {if(it != null) ConcurrentHashMap(it) else ConcurrentHashMap()})
+        this.pendingSponsorScanQueue = loadSavedValue(KEY_PENDING_SPONSOR_SCAN, SerializedPendingSponsorScanQueue(), {if(it != null) ConcurrentLinkedQueue(it) else ConcurrentLinkedQueue()})
+        this.inProcessScan = loadSavedValue(KEY_IN_PROCESS_SPONSOR_SCAN, SerializedInProcessScan(), {if(it != null) ArrayList(it) else mutableListOf()})
+    }
+
+    fun <T> loadSavedValue(key: String, typeToken: TypeToken<T>, supplier: (T?) -> T) : T {
+        val existing:T? = Common.gson.fromJson(sharedPreferences.getString(key, ""), typeToken.type);
+        return supplier(existing);
     }
 
     fun persistAlfioConfigurations() {
-        sharedPreferences.edit().putString(KEY_ALFIO_CONFIGURATIONS, Common.gson.toJson(configurations)).apply();
+        persist(configurations, KEY_ALFIO_CONFIGURATIONS);
     }
 
     fun saveAlfioConfiguration(alfioConfiguration: AlfioConfiguration) {
@@ -69,8 +78,32 @@ object DataService {
 
     fun blacklistedConfigurationsCount() = blacklist.size;
 
+    fun getPendingSponsorScan(): Map<AlfioConfiguration, List<String>> {
+        val extracted : MutableList<Pair<AlfioConfiguration, String>> = mutableListOf();
+        while(pendingSponsorScanQueue.isNotEmpty()) {
+            val e = pendingSponsorScanQueue.poll() ?: break;
+            extracted.add(e);
+        }
+        inProcessScan.addAll(extracted);
+
+        return extracted.groupBy({ it.first }, {it.second});
+    };
+
+    fun enqueueSponsorScan(configuration: AlfioConfiguration, code: String) {
+        pendingSponsorScanQueue.offer(configuration to code);
+        persist(pendingSponsorScanQueue, KEY_PENDING_SPONSOR_SCAN);
+    }
+
+    private fun persist(obj: Any, key: String) {
+        sharedPreferences.edit().putString(key, Common.gson.toJson(obj)).apply();
+    }
 }
+
 private val KEY_ALFIO_CONFIGURATIONS = "alfio-configurations"
+private val KEY_PENDING_SPONSOR_SCAN = "alfio-pending-sponsor-scan"
+private val KEY_IN_PROCESS_SPONSOR_SCAN = "alfio-in-process-sponsor-scan"
 private val KEY_VIBRATION_FEEDBACK_ENABLED = "enable_vibration_feedback"
 private val KEY_SHAKE_TO_CHECK_IN_ENABLED = "enable_shake_to_checkin"
-class SerializedConfigurations: TypeToken<Map<String, AlfioConfiguration>>();
+class SerializedConfigurations: TypeToken<MutableMap<String, AlfioConfiguration>>();
+class SerializedPendingSponsorScanQueue: TypeToken<Queue<Pair<AlfioConfiguration, String>>>();
+class SerializedInProcessScan: TypeToken<MutableList<Pair<AlfioConfiguration, String>>>();
