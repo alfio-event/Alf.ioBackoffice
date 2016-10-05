ccc510412600a1e36465c99fa441a0d206bf5092
Celestino Bellone <celestino@exteso.com>
2016-10-05 23:19:43 +0200

    first GUI

--- app/pages/account-selection/account-selection.component.ts
+++ app/pages/account-selection/account-selection.component.ts
@@ -7,22 +7,64 @@ import { View } from "ui/core/view";
 
 import { Account } from "../../shared/account/account";
 import { AccountService } from "../../shared/account/account.service";
+var barcodescanner = require("nativescript-barcodescanner");
 
 @Component({
     selector: "my-app",
     providers: [AccountService],
     templateUrl: "pages/account-selection/account-selection.html",
-    styleUrls: [],
+    styleUrls: ["pages/account-selection/account-selection-common.css"],
 })
 export class AccountSelectionComponent implements OnInit {
-    accounts: Array<Account>;
+    accounts: Array<Account> = [];
+    isLoading: boolean;
 
     constructor(private router: Router, private accountService: AccountService, private page: Page) {
         this.accounts = accountService.getRegisteredAccounts();
+        console.log("init completed. Loaded "+ this.accounts.length + " accounts");
+        console.log(JSON.stringify(this.accounts));
     }
 
     ngOnInit() {
-        this.page.actionBarHidden = true;        
+        this.page.actionBarHidden = true;
+        this.isLoading = false;
     }
-    
+
+    hasAccounts() : boolean {
+        return this.accounts.length > 0;
+    }
+
+    requestQrScan() {
+        this.isLoading = true;
+        barcodescanner.scan({
+            formats: "QR_CODE",   // Pass in of you want to restrict scanning to certain types 
+            cancelLabel: "Stop scanning", // iOS only, default 'Close' 
+            message: "Go scan something", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.' 
+            preferFrontCamera: false,     // Android only, default false 
+            showFlipCameraButton: true,   // Android only, default false (on iOS it's always available) 
+            orientation: "portrait"      // Android only, optionally lock the orientation to either "portrait" or "landscape" 
+        }).then((result) => {
+                this.isLoading = true;
+                let scanResult = JSON.parse(result.text);
+                this.accountService.registerNewAccount(scanResult.baseUrl, scanResult.username, scanResult.password)
+                    .subscribe((newItem) => {
+                        console.log("success!");
+                        if(newItem != null) {
+                            console.log("pushing itemResult");
+                            this.accounts.push(newItem);
+                            console.log("done. current list size: "+this.accounts.length);
+                        }
+                        this.isLoading = false;
+                    }, () => {
+                        alert("error")
+                        this.isLoading = false;
+                    });
+                
+            },(error) => {
+                console.log("No scan: " + error);
+                this.isLoading = false;
+            }
+        );
+    }
+
 }
--- app/shared/account/account.service.ts
+++ app/shared/account/account.service.ts
@@ -4,58 +4,108 @@ import { Observable } from "rxjs/Rx";
 import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
 var appSettings = require("application-settings");
-var btoa = require("btoa");
 const ACCOUNTS_KEY = "ALFIO_ACCOUNTS";
 
 import { Account, AccountType, EventConfiguration } from "./account";
 
 @Injectable()
-export class AccountService implements OnInit {
+export class AccountService {
     private accounts: Map<string, Account> = new Map<string, Account>();
 
-    constructor(private http: Http) {}
-    
-    ngOnInit() {
+    constructor(private http: Http) { 
         this.loadSavedAccounts();
     }
 
     public registerNewAccount(url: string, username: string, password: string) {
         let headers = new Headers();
-        headers.append('Authorization', 'Basic '+ btoa(username + ":" + password));
+        let token = this.encodeBase64(username + ":" + password);
+        headers.append("Authorization", "Basic " + token);
+        console.log("calling: " + url);
         return this.http.get(url + "/admin/api/user-type", {
-                headers: headers
-            }).map(response => response.json())
-            .do(data => {
-                let account = new Account();
-                account.url = url;
-                account.username = username;
-                account.password = password;
-                account.accountType = data === "SPONSOR" ? AccountType.SPONSOR : AccountType.STAFF;
-                account.configurations = [];
-                let newAccountKey = account.getKey();
-                if(!this.accounts.has(newAccountKey)) {
-                    this.accounts.set(newAccountKey, account);
-                    this.persistAccounts();
-                    return account;
-                } else {
-                    return this.accounts.get(newAccountKey);
-                }
-            }).catch(error => {
-                console.log(JSON.stringify(error.json()));
-                return Observable.throw(error);
-            });
+            headers: headers
+        })
+        .map(response => response.text())
+        .map(data => {
+            let account = new Account();
+            account.url = url;
+            account.username = username;
+            account.password = password;
+            account.accountType = data === "SPONSOR" ? AccountType.SPONSOR : AccountType.STAFF;
+            account.configurations = [];
+            let newAccountKey = account.getKey();
+            if (!this.accounts.has(newAccountKey)) {
+                console.log("account does not exist. Inserting...");
+                this.accounts.set(newAccountKey, account);
+                this.persistAccounts();
+                console.log("accounts persisted.");
+                return account;
+            } else {
+                console.log("Account exists. Doing nothing");
+                return null;
+            }
+        }).catch(error => {
+            console.log("got error! ");
+            console.log(JSON.stringify(error));
+            return Observable.throw(error);
+        });
     }
 
     public getRegisteredAccounts(): Array<Account> {
-        return this.accounts.values();
+        let result = new Array<Account>();
+        for(var value of this.accounts.values()) {
+            result.push(value);
+        }
+        return result;
     }
 
     private loadSavedAccounts() {
-       this.accounts = JSON.parse(appSettings.getString(ACCOUNTS_KEY, "{}"));
+        let savedData = appSettings.getString(ACCOUNTS_KEY, "--");
+        console.log("loaded saved data: "+savedData);
+        if(savedData !== "--") {
+            console.log("parsing saved data...");
+            this.accounts = <Map<string, Account>>JSON.parse(savedData);
+        }
     }
 
     private persistAccounts() {
         appSettings.setString(ACCOUNTS_KEY, JSON.stringify(this.accounts));
+    }
+
+    private encodeBase64(str: string) {
+        var padChar = '=';
+        var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+        var getByte = function (s, i) {
+            var cc = s.charCodeAt(i);
+            if (cc > 255) {
+                throw "INVALID_CHARACTER_ERR: DOM Exception 5";
+            }
+            return cc;
+        };
+        var b10, i;
+        var b64Chars = [];
+        var iMax = str.length - str.length % 3;
+        for (i = 0; i < iMax; i += 3) {
+            b10 = (getByte(str, i) << 16) |
+                (getByte(str, i + 1) << 8) |
+                getByte(str, i + 2);
+            b64Chars.push(alpha.charAt(b10 >> 18));
+            b64Chars.push(alpha.charAt((b10 >> 12) & 0x3F));
+            b64Chars.push(alpha.charAt((b10 >> 6) & 0x3f));
+            b64Chars.push(alpha.charAt(b10 & 0x3f));
+        }
+        switch (str.length - iMax) {
+            case 1:
+                b10 = getByte(str, i) << 16;
+                b64Chars.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
+                    padChar + padChar);
+                break;
+            case 2:
+                b10 = (getByte(str, i) << 16) | (getByte(str, i + 1) << 8);
+                b64Chars.push(alpha.charAt(b10 >> 18) + alpha.charAt((b10 >> 12) & 0x3F) +
+                    alpha.charAt((b10 >> 6) & 0x3F) + padChar);
+                break;
+        }
+        return b64Chars.join('');
     }
 
 }
