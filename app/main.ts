3e5e621e10a36a711ac682d9daad50e027aa1bce
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-02-02 15:37:06 +0100

    use FAB for adding new accounts

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,9 @@ import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
 import { setStatusBarColors } from "./utils/status-bar-util";
 import {forcePortraitOrientation} from "./utils/orientation-util"
+import { registerElement } from "nativescript-angular/element-registry";
+
+registerElement("Fab", () => require("nativescript-floatingactionbutton").Fab);
 
 setStatusBarColors();
 forcePortraitOrientation();
