99aa630b64394a39d128e5d75256c780641ae951
Sylvain Jermini <sylvain.jermini@syjer.com>
2017-02-03 13:34:40 +0100

    fix api

--- app/shared/scan/sponsor-scan.service.ts
+++ app/shared/scan/sponsor-scan.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Observable, Subject } from 'rxjs';
 import { SponsorScan } from "./sponsor-scan";
+import { Http, Headers, Response } from "@angular/http";
+import { Account } from "../account/account";
 
 @Injectable()
 export class SponsorScanService {
     
+    constructor(private http: Http) {
+    }
 
     public scan(eventKey: string, uuid: string) : void {
     }
 
-    public getForEvent(eventKey: string) : Observable<Array<SponsorScan>> {
+    public getForEvent(eventKey: string, account: Account) : Observable<Array<SponsorScan>> {
         let subj = new Subject<Array<SponsorScan>>();
         return subj.asObservable();
     }
