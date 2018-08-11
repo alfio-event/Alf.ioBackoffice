933a40e8fe37e77814cb1b2e0ba7948a179eebfa
Celestino Bellone <celestino@swicket.io>
2018-08-11 18:33:26 +0200

    temporarily remove image loader, since it can cause application crashes

--- app/pages/event-header/event-header.component.ts
+++ app/pages/event-header/event-header.component.ts
@@ -5,22 +5,20 @@ import {
     Input,
     OnInit,
     Output} from '@angular/core';
-import { ImageService } from "../../shared/image/image.service";
 import { Account, EventConfiguration } from "../../shared/account/account";
 
 @Component({
     moduleId: module.id,
     selector: "event-header",
-    providers: [ImageService],
-    template: `<GridLayout columns="70, *, 10" *ngIf="event">
-                    <Image col="0" [src]="eventImage" stretch="aspectFit" width="70" height="70"></Image>
+    template: `<GridLayout columns="35, *, 35" *ngIf="event">
+                    <Label text="&#xf1df;" col="0" class="icon icon-1-5x person-icon text-primary text-center"></Label>
                     <StackLayout col="1" class="medium-spacing" orientation="vertical" (tap)="select()">
                         <Label [text]="event.name" class="eventName" textWrap="true"></Label>
                         <Label text="{{ event.begin | date:'short' }} - {{ event.end | date:'short' }}" *ngIf="!event.oneDay"></Label>
                         <Label text="{{ event.begin | date:'shortDate' }} {{ event.begin | date:'shortTime' }} - {{ event.end | date:'shortTime' }}" *ngIf="event.oneDay"></Label>
                         <Label [text]="event.location"></Label>
                     </StackLayout>
-                    <Label text="&#xf154;" class="icon icon-1-5x person-icon text-center"></Label>
+                    <Label text="&#xf2fb;" col="2" class="icon icon-1-5x person-icon text-center"></Label>
                 </GridLayout>`,
     styleUrls: ['./event-header-common.css']
 })
@@ -34,15 +32,11 @@ export class EventHeaderComponent implements OnInit {
 
     eventImage: string;
 
-    constructor(private imageService: ImageService) {
+    constructor() {
     }
 
     ngOnInit(): void {
         this.isLoading = true;
-        this.imageService.getImage(this.account, this.event).subscribe(imgUrl => {
-            this.eventImage = imgUrl;
-            this.isLoading = false;
-        })
     }
 
     select(): void {
--- app/pages/ios-account-icon/ios-account-icon.component.ts
+++ app/pages/ios-account-icon/ios-account-icon.component.ts
@@ -3,14 +3,12 @@ import {
     Injectable,
     Input,
     OnInit} from '@angular/core';
-import { ImageService } from "../../shared/image/image.service";
 import { Account } from "../../shared/account/account";
 import { Observable, Subject } from 'rxjs';
 
 @Component({
     moduleId: module.id,
     selector: "ios-account-icon",
-    providers: [ImageService],
     template: `<Label [text]="text" class="delete-item icon icon-1-5x" (tap)="select(account)" [class.icon-forward]="!editEnabled" [class.icon-trash]="editEnabled"></Label>`,
     styleUrls: ['./ios-account-icon-common.css']
 })
