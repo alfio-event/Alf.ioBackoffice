001f92cf40439939a437e87cd38615e3e9fa65c1
Celestino Bellone <celestino@swicket.io>
2020-11-15 18:00:11 +0100

    update nativescript version

---
+++ nativescript.config.ts
@@ -0,0 +1,18 @@
+import { NativeScriptConfig } from '@nativescript/core'
+
+export default {
+  id: 'alfio.scan',
+  main: 'app/main.ts',
+  appResourcesPath: 'App_Resources',
+  webpackConfigPath: 'webpack.config.js',
+  ios: {
+    discardUncaughtJsExceptions: true,
+  },
+  android: {
+    discardUncaughtJsExceptions: true,
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+    suppressCallJSMethodExceptions: false,
+  },
+  appPath: 'src',
+} as NativeScriptConfig
