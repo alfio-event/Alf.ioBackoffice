f3822b4ca7739b534a0e61d16b9e1170959b8ee5
Celestino Bellone <3385346+cbellone@users.noreply.github.com>
2022-08-15 15:35:48 +0200

    update config files following new NS conventions

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -4,15 +4,15 @@ export default {
   id: 'alfio.scan',
   main: 'src/main.ts',
   appResourcesPath: 'App_Resources',
-  webpackConfigPath: 'webpack.config.js',
-  ios: {
-    discardUncaughtJsExceptions: true,
-  },
+  // webpackConfigPath: 'webpack.config.js',
+  // ios: {
+  //   discardUncaughtJsExceptions: true,
+  // },
   android: {
-    discardUncaughtJsExceptions: true,
+    // discardUncaughtJsExceptions: true,
     v8Flags: '--expose_gc',
     markingMode: 'none',
-    suppressCallJSMethodExceptions: false,
+    // suppressCallJSMethodExceptions: false,
   },
   appPath: 'src',
 } as NativeScriptConfig;
